c89018f140e7c1b864b7d88f929fe28b58a78f67
zambro <ramk@asu.edu>
2017-03-31 11:22:15 -0700

    Common character counter between two strings

--- src/com/codefights/arcade/CommonCharacterCounter.java
+++ src/com/codefights/arcade/CommonCharacterCounter.java
@@ -5,6 +5,14 @@ import java.util.Map;
 
 /**
  * Created by zambro on 3/30/17.
+ * Given two strings, find the number of common characters between them.
+
+ Example
+
+ For s1 = "aabcc" and s2 = "adcaa", the output should be
+ commonCharacterCount(s1, s2) = 3.
+
+ Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 public class CommonCharacterCounter {
 
