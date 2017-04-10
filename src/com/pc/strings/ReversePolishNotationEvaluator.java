0bd5480669910a961a6baaa3ebb3b7c2ff4e0222
zambro <ramk@asu.edu>
2017-04-09 17:30:59 -0700

    Sum two linkedlist and palindrome validator

--- src/com/pc/strings/ReversePolishNotationEvaluator.java
+++ src/com/pc/strings/ReversePolishNotationEvaluator.java
@@ -4,6 +4,11 @@ import java.util.Stack;
 
 /**
  * Created by zambro on 3/7/17.
+ * Evaluate the value of an arithmetic expression in Reverse Polish Notation. Valid operators are +, -, *, /. Each operand may be an integer or another expression. For example:
+
+ ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+ ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+
  */
 public class ReversePolishNotationEvaluator {
 
